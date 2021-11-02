3e6769ba4b3e4542442d7d0c735305efad2dd140
satyamkumar94 <84905624+satyamkumar94@users.noreply.github.com>
2021-11-02 10:36:20 +0530

    Delete 2_practice.c

--- 2_practice.c
+++
@@ -1,8 +0,0 @@
-// evaluate 3*x/y-z+k,x=2,y=3,z=3,k=1
-#include <stdio.h>
-int main()
-{
-    int x = 2, y = 3, z = 3, k = 1;
-    int result = 3 * x / y - z + k;
-    printf("the value of result is %d", result);
-}
